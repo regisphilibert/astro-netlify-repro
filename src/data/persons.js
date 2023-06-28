5754c4886375c027248ee25fc614b65c66bafa1a
Regis Philibert <login@regisphilibert.com>
2023-06-28 15:18:11 -0400

    first commit

---
+++ src/data/persons.js
@@ -0,0 +1,10 @@
+export default [{"id":1,"first_name":"Royce","last_name":"Shyres","email":"rshyres0@kickstarter.com","ip_address":"130.246.137.131"},
+{"id":2,"first_name":"Lonnard","last_name":"Ransome","email":"lransome1@samsung.com","ip_address":"142.125.172.75"},
+{"id":3,"first_name":"Sherwynd","last_name":"Wickersham","email":"swickersham2@shutterfly.com","ip_address":"195.5.100.127"},
+{"id":4,"first_name":"Rem","last_name":"Petche","email":"rpetche3@webs.com","ip_address":"124.128.80.192"},
+{"id":5,"first_name":"Elicia","last_name":"Tabour","email":"etabour4@discuz.net","ip_address":"173.179.198.184"},
+{"id":6,"first_name":"Tove","last_name":"Hallas","email":"thallas5@guardian.co.uk","ip_address":"191.201.9.210"},
+{"id":7,"first_name":"Eddie","last_name":"Grigorushkin","email":"egrigorushkin6@posterous.com","ip_address":"90.207.220.188"},
+{"id":8,"first_name":"Cosme","last_name":"McSparran","email":"cmcsparran7@barnesandnoble.com","ip_address":"24.113.224.84"},
+{"id":9,"first_name":"Chrissy","last_name":"Roubeix","email":"croubeix8@imdb.com","ip_address":"99.15.135.134"},
+{"id":10,"first_name":"Melantha","last_name":"Tomkowicz","email":"mtomkowicz9@si.edu","ip_address":"113.132.187.195"}]
---
+++ src/data/site.js
@@ -0,0 +1,3 @@
+export default {
+  title: 'Repro Netlify Issue'
+}
