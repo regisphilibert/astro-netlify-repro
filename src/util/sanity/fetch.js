8c81f377b038572c3b4847d4b3d1f7b06e3327e2
Regis Philibert <login@regisphilibert.com>
2023-06-29 15:12:53 -0400

    Add Eleventy Fetch

--- src/util/sanity/fetch.js
+++ src/util/sanity/fetch.js
@@ -1,3 +1,4 @@
+import EleventyFetch from '@11ty/eleventy-fetch'
 import { createClient } from '@sanity/client'
 import site, { sanity } from '@data/site';
 
@@ -10,6 +11,26 @@ const client = createClient({
   apiVersion, // use current date (YYYY-MM-DD) to target the latest API version
   token
 })
+
+const cachedFetch = async function(params_input) {
+  const { query, params = {}, hash = "unique" } = params_input
+  let url = `https://${projectId}.api.sanity.io/v${apiVersion}/data/query/production#${hash}`
+  const cacheDir = site.env == "dev" ? ".cache" : "/tmp/.cache/"
+  const response = await EleventyFetch(url, {
+    duration: site.env ==  "dev" ? "1h" : "5m", // save for 5m in production
+    type: "json",    // we’ll parse JSON for you
+    directory: cacheDir,
+    fetchOptions: {
+      method: "POST",
+      body: JSON.stringify({query, params}),
+      headers: {
+        // lol
+        "Content-Type":"application/groq"
+      }
+    }
+  }).then((res) => res.result );
+  return response
+}
 
 const normalFetch = async function(params_input) {
   const { query, params = {} } = params_input
@@ -28,5 +49,9 @@ export default async function(params_input) {
 
   useCache = false
   
-  return await normalFetch(params_input)
+  if(useCache) {
+    return await cachedFetch(params_input)
+  } else {
+    return await normalFetch(params_input)
+  }
 }
